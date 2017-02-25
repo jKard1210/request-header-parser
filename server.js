08676ac211c32a0939b9c805fa4f8f7ffebdb9fc
James Kardatzke <kardatzkejames@yahoo.com>
2017-02-25 05:16:46 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express()
+
+app.get('/whoami', function (req, res) {
+  var ip = req.connection.remoteAddress;
+  var lang = req.headers["accept-language"];
+  var os = req.headers["user-agent"];
+  
+  var data = {"ip": ip, "lang": lang, "Operating System": os};
+  
+  res.send(data)
+})
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 3000!')
+})
